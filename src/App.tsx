36d32cf2fe7fb91904cb6056797311f7d219dce8
Quang Tuyen Dinh <qtuyen43@gmail.com>
2025-11-19 07:52:38 +0100

    feat: init app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Puzzle from './components/Puzzle';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
