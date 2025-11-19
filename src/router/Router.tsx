59dcc581c1907e531462680a74c36913b8ebc7dd
qt-inbolt <quangtuyen.dinh@inbolt.fr>
2025-11-19 17:26:14 +0100

    fix: SPA 404 on refresh via vercel rewrites and root route

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,11 +1,11 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Puzzle from '../components/Puzzle';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/puzzle" replace />} />
+        <Route path="/" element={<Puzzle />} />
         <Route path="/puzzle" element={<Puzzle />} />
       </Routes>
     </BrowserRouter>
